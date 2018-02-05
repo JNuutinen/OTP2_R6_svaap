55a0de2464acf837a46c201518fee3cc659c22d7
Jerry Hällfors <jerry.hallfors@gmail.com>
2018-02-05 13:19:56 +0200

    Weaponi kommentoitu pois, kunnes komponentit toimii.

--- src/main/java/model/Weapon.java
+++ src/main/java/model/Weapon.java
@@ -1,12 +1,12 @@
 package model;
 
-public class Weapon extends Component{
+public class Weapon {
     int damage;
     int fireRate;
     int direction = 1;
     Unit shooter;
-
-    public Weapon(int unitX, int unitY, double width, double height, String imagePath, int damage, int fireRate) {
+ /*
+    public Weapon(double unitX, double unitY, double width, double height, String imagePath, int damage, int fireRate) {
         super(unitX, unitY, width, height, imagePath);
         spawn(unitX, unitY, width, height, imagePath);
         this.damage = damage;
@@ -15,4 +15,5 @@ public class Weapon extends Component{
     public void shoot() {
         Projectile projectile = new Projectile(damage, direction, shooter);
     }
+    */
 }
